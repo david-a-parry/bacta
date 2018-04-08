e1d7704aa7e172e4478055b16573049828882dfe
gantzgraf <d.a.parry@leeds.ac.uk>
2018-04-08 20:25:13 +0100

    add variant checking script

--- setup.py
+++ setup.py
@@ -12,10 +12,12 @@ setup(
     license='GPLv3',
     install_requires=[
           'pysam>=0.12.0',
+          'parse_vcf>=0.2.1', #for bin/var_vs_contam
       ],
     scripts = ["bin/bacta", "bin/contam_reads_to_fastq", "bin/summary_to_bed",
+               "bin/var_vs_contam",
                "bin/test_bacta_clipping_thresholds", 
-                "bin/test_bacta_expect_thresholds",
+               "bin/test_bacta_expect_thresholds",
         ],
     classifiers = [
         "Programming Language :: Python :: 3",
