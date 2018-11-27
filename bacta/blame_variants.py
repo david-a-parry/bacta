f69652c0af3632e82a8debc89362c3cede768e5d
David A. Parry <david.parry@igmm.ed.ac.uk>
2018-11-27 17:41:06 +0000

    remove extraneuous whitespace

--- bacta/blame_variants.py
+++ bacta/blame_variants.py
@@ -8,11 +8,11 @@ from .alignment_file_utils import get_bamfile
 from parse_vcf import VcfReader
 
 class BlameVariants(object):
-    ''' Identify the number of contaminant reads versus 
+    ''' Identify the number of contaminant reads versus
         non-contaminant carrying given variants.
     '''
 
-    def __init__(self, contaminants, variants, bam, bed=None, output=None, 
+    def __init__(self, contaminants, variants, bam, bed=None, output=None,
                  mapq=0, contam_ratio=0.0, quiet=False, debug=False):
         self.logger = self._get_logger(quiet, debug)
         self.mapq = mapq
@@ -39,11 +39,11 @@ class BlameVariants(object):
                 indices[field] = i
             for line in infile:
                 cols = line.rstrip().split()
-                if self.mapq and int(cols[indices['OLD_MAPQ']]) < self.mapq: 
+                if self.mapq and int(cols[indices['OLD_MAPQ']]) < self.mapq:
                     self.contam_below_mapq.add(cols[indices['ID']])
                 else:
                     self.contam_ids.add(cols[indices['ID']])
-        infile.close()  
+        infile.close()
 
     def _get_logger(self, quiet=False, debug=False):
         logger = logging.getLogger("Blame Variants")
@@ -66,7 +66,7 @@ class BlameVariants(object):
             self.outfile.close()
 
     def assess_variants(self):
-        if self.output is not None: 
+        if self.output is not None:
             self.outfile = open(self.output, 'wt')
         else:
             self.outfile = sys.stdout
@@ -79,19 +79,19 @@ class BlameVariants(object):
 
     def write_header(self):
         self.vcf.header.add_header_field(
-                name='BactaContamSupport', 
-                dictionary={'Number' : 'A', 
+                name='BactaContamSupport',
+                dictionary={'Number' : 'A',
                              'Type' : 'Integer',
                              'Description' : 'Number of contaminant reads ' +
                                              'supporting ALT allele according'+
                                              ' to BACTA'''},
                 field_type='INFO')
         self.vcf.header.add_header_field(
-                name='BactaNonContamSupport', 
-                dictionary={'Number' : 'A', 
+                name='BactaNonContamSupport',
+                dictionary={'Number' : 'A',
                              'Type' : 'Integer',
                              'Description' : 'Number of non-contaminant reads'+
-                                             ' supporting ALT allele '+ 
+                                             ' supporting ALT allele '+
                                              'according to BACTA'},
                 field_type='INFO')
         self.outfile.write(str(self.vcf.header))
@@ -102,7 +102,7 @@ class BlameVariants(object):
         else:
             bedfile = open(self.bed, 'rt')
         for line in bedfile:
-            if line[0] == '#': 
+            if line[0] == '#':
                 continue
             cols = line.rstrip().split()
             self.vcf.set_region(cols[0], int(cols[1]), int(cols[2]))
@@ -121,9 +121,9 @@ class BlameVariants(object):
             pileup_iter = self.bamfile.pileup(alt.CHROM, alt.POS-1, alt.POS)
             supporting_contam = 0
             supporting_non_contam = 0
-            for x in pileup_iter: 
+            for x in pileup_iter:
                 #pysam positions are 0-based
-                if x.reference_pos != var.POS - 1: 
+                if x.reference_pos != var.POS - 1:
                     continue
                 for p in x.pileups:
                     if p.is_del or p.is_refskip:
@@ -132,7 +132,7 @@ class BlameVariants(object):
                         continue
                     seq = p.alignment.seq[p.query_position:p.query_position + 1]
                     if seq == alt.ALT:
-                        query_name = p.alignment.query_name 
+                        query_name = p.alignment.query_name
                         if query_name in self.contam_ids:
                             supporting_contam += 1
                         elif query_name not in self.contam_below_mapq:
@@ -140,7 +140,7 @@ class BlameVariants(object):
                 break #can skip now we've hit our SNV position
             if supporting_contam:
                 dp = supporting_non_contam + supporting_contam
-                if supporting_contam/dp >= self.contam_ratio: 
+                if supporting_contam/dp >= self.contam_ratio:
                     write_record=True
             contam.append(supporting_contam)
             non_contam.append(supporting_non_contam)
